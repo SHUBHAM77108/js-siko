fb0054c2ec9bad02bd6052bc60d6420afa697e60
Shubham Vishwakarma <100425143+SHUBHAM77108@users.noreply.github.com>
2023-10-10 06:26:11 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = true
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+// "shubham" => NaN
+// 
+
+let isLoggedIn = "shubham"
+;
+
+let booleanIsLoggedIn  =  Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "shubham" => ture
+
+let someNUmber = 33
+let stringNumber = String(someNUmber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
