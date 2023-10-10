c87b1c7cfd2a19d541c766b000bf59c70b743960
Shubham Vishwakarma <100425143+SHUBHAM77108@users.noreply.github.com>
2023-10-10 09:10:22 +0000

    Add conversion operation file

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -17,7 +17,7 @@ let isLoggedIn = "shubham"
 ;
 
 let booleanIsLoggedIn  =  Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 // "" => false
@@ -25,5 +25,47 @@ console.log(booleanIsLoggedIn)
 
 let someNUmber = 33
 let stringNumber = String(someNUmber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+
+// ************************** Operstion ********************
+let value = 3 
+let negvalue = -value
+// console.log(negvalue);
+// console.log(typeof negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = " shubham"
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2")
+// console.log("1"+ 2 +2)
+// console.log(1 + 2 + "2")
+
+// console.log((3 + 4) * 5 % 3)
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounterPostfix =100
+let gamecounterPrefix = 100
+
+gameCounterPostfix++;
+++gamecounterPrefix;
+
+console.log(gameCounterPostfix)
+console.log(gamecounterPrefix)
