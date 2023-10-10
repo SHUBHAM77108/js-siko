0c38e1672c7372ffd7ed316b655109b11645e320
Shubham Vishwakarma <100425143+SHUBHAM77108@users.noreply.github.com>
2023-10-10 05:44:29 +0000

    Add datatypes file

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+// alert (3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 
+    3) //code readability should be high
+    console.log("shubham")
+
+    let name = "shubham" // String 
+    let age = 18 
+    let isLoggedIn = false // boolean
+    let state; //ndefined value
+
+    // number => 2 to power 53
+    // bigint 
+    // String => ""
+    // boolean => true/false
+    // null => standlone value  // *null is object
+    // undefined value => 
+    // Symbol => unique
+
+
+    // object
+
+    console.log(typeof undefined) // undefined
+    console.log(typeof null) // object
