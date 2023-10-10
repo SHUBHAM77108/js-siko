a5fc0a2e408e8de2e800abab6e90dcdf6a1420e8
Shubham Vishwakarma <100425143+SHUBHAM77108@users.noreply.github.com>
2023-10-10 05:16:25 +0000

    variable file add

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const accountID = 12345
+let accountEmail = "subham@gmail.com"
+var accountPassword = "11234"
+accountCity = "Mumbai"
+let accountState;
+
+// accountID = 2 // not allowwd
+
+accountEmail = "acb@gmail.com"
+accountPassword = "13"
+accountCity = "Mubai"
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
