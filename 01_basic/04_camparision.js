3c11b42a2e35b56a14d2248c1c6bc1b4d15e92a1
Shubham Vishwakarma <100425143+SHUBHAM77108@users.noreply.github.com>
2023-10-13 16:16:26 +0000

    add 04_camparision

---
+++ 01_basic/04_camparision.js
@@ -0,0 +1,9 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+console.log("2" > 1); //output true because "2" or "02 " is automatically convert by JS in number 
+console.log("02" > 1);
