d1f25dcfbc1efeb8e062985eb8618b09fcde9e36
Shubham Vishwakarma <100425143+SHUBHAM77108@users.noreply.github.com>
2023-12-31 06:06:48 +0000

    Add file iterations

---
+++ 01_basic/iterations_HighOrderArray_Loops.js
@@ -0,0 +1,26 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    swift: "swifr by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key}- shortcut is for -${myObject[key]}`)
+}
+
+const progtamming = ["js", "rg", "py", "java", "C++"]
+
+for (const key in progtamming){
+    console.log(progtamming[key])
+}
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United states of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+for (const key in map){
+    console.log(key)
+}
