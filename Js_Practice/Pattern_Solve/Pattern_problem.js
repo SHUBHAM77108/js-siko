ce011f9c72454b3cd281ee6d3bb27e0f0e53b9a7
Shubham Vishwakarma <100425143+SHUBHAM77108@users.noreply.github.com>
2024-01-12 15:33:28 +0000

    Solve 5 question in pattern printing problem

---
+++ Js_Practice/Pattern_Solve/Pattern_problem.js
@@ -0,0 +1,53 @@
+// first 
+
+// *
+// **
+// ***
+// ****
+
+// let n = 4; 
+// let string = ""
+
+// for(let i = 1; i <= 3; i++){
+//     for(let j = 1; j <= n; j++){
+//         string += j
+//     }
+//     string += "\n"
+// }
+// console.log(string);
+
+
+// 2 Pattern
+
+// 1
+// 12
+// 123
+// 1234
+
+// let n = 4; 
+// let string = ""
+
+// for(let i = 1; i <= n; i++){
+//     for(let j = 1; j <= i; j++){
+//         string += "*"
+//     }
+//     string += "\n"
+// }
+// console.log(string);
+
+// 3 Pattern
+// ****
+// ***
+// **
+// *
+
+// let n = 4; 
+// let string = ""
+
+// for(let i = 1; i <= n; i++){
+//     for(let j = 4; j >= i; j--){
+//         string += "*"
+//     }
+//     string += "\n"
+// }
+// console.log(string);
