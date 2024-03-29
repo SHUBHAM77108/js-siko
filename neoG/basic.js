fa7ae4e0f546d312e9c90b8c97d3d038c93f7615
Shubham Vishwakarma <100425143+SHUBHAM77108@users.noreply.github.com>
2024-03-29 11:36:49 +0000

    new commit

---
+++ neoG/basic.js
@@ -0,0 +1,110 @@
+let myLastName = "Balika"
+console.log(myLastName)
+console.log("-------------------")
+let oneChar = "C"
+console.log(oneChar)
+console.log("-------------------")
+let longStr = "Basics build strong foundation"
+console.log(longStr)
+console.log("-------------------")
+let strThree = "Three"
+let anotherStr = strThree
+console.log(anotherStr)
+console.log("-------------------")
+let firstPart = "pqr"
+let secondPart = "stu"
+let combined = firstPart + secondPart
+console.log(combined)
+
+console.log("-------------------")
+
+let age = 23
+console.log(age)
+console.log("-------------------")
+let isSmart = "true"
+console.log(isSmart)
+let isOld = "false"
+console.log(isOld)
+
+console.log("-------------------")
+// hi shubham 
+// console.log("this will not be printed 2")
+console.log("Practising comments is even better")
+
+let myCity = "Bangalore"
+console.log(myCity)
+console.log("-------------------")
+let aloneChar = "M"
+console.log(aloneChar)
+console.log("-------------------")
+let longLine = "Things that take time to build, stay longer"
+console.log(longLine)
+console.log("-------------------")
+let justAnotherVariable = "something"
+let yetAnotherVariable = justAnotherVariable
+console.log(yetAnotherVariable)
+console.log("-------------------")
+let partUno = "uvw"
+let partDes = "xyz"
+let lastsix = partUno + partDes
+console.log(lastsix)
+
+console.log("--------NEW Part 2-------")
+
+let day1 = "Monday, "
+let day2 = "Tuesday, "
+let day3 = "Wednesday"
+
+let weekDay = day1 + day2 + day3
+console.log(weekDay)
+console.log("-------------------")
+
+let employee1 = "shubham"
+let employee2 = "Abhishek"
+let employee3 = "Aditya"
+
+let message1 = "Hola, " + employee1
+let message2 = "Hola, " + employee2
+let message3 = "Hola, " + employee3
+
+console.log(message1)
+console.log(message2)
+console.log(message3)
+
+console.log("-------------------")
+
+let age1 = 15
+let age2 = 25
+let age3 = 35
+
+console.log("The first person's age is " + age1)
+console.log("The second person's age is " + age2)
+console.log("The third person's age is " + age3)
+
+console.log("--------NEW Part 3-------")
+
+let destinationOne = "jaipur"
+let destinationOneAttraction1  = "Hawa Mahal"
+let destinationOneAttraction2 = "Amber Fort"
+let destinationOneAttraction3 = "City Palace"
+let destinationOneRating = 8.5
+let destinationOneVisitorsPerYear = 3000000
+let destinationOneDescription = "Jaipur, the Pink City, is known for its magnificent palaces, vibrant culture, and exquisite handicrafts."
+
+console.log(destinationOne)
+console.log(destinationOneRating)
+console.log(destinationOneAttraction1,destinationOneAttraction2,destinationOneAttraction3)
+console.log(destinationOneVisitorsPerYear)
+console.log(destinationOneDescription)
+
+console.log("--------NEW Part 3-------")
+
+let employeeOneName = "Ravi Verma"
+let employeeOnePosition = "Software Developer"
+let employeeOneSalary = "60000"
+let employeeOneYearsOfExperience = 4
+
+let employeeTwoName = "Priya Sharma"
+let employeeTwoPosition = "Marketing Manager"
+let employeeTwoSalary = "70000"
+let employeeTwoYearsOfExperience = 6
