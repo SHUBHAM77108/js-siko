8b02d1b31c9de2c9802bfc6abdd1e75b0be92475
Shubham Vishwakarma <100425143+SHUBHAM77108@users.noreply.github.com>
2024-03-29 16:05:47 +0000

    commit

--- neoG/basic.js
+++ neoG/basic.js
@@ -97,7 +97,7 @@ console.log(destinationOneAttraction1,destinationOneAttraction2,destinationOneAt
 console.log(destinationOneVisitorsPerYear)
 console.log(destinationOneDescription)
 
-console.log("--------NEW Part 3-------")
+console.log("--------NEW Part 4-------")
 
 let employeeOneName = "Ravi Verma"
 let employeeOnePosition = "Software Developer"
@@ -108,3 +108,229 @@ let employeeTwoName = "Priya Sharma"
 let employeeTwoPosition = "Marketing Manager"
 let employeeTwoSalary = "70000"
 let employeeTwoYearsOfExperience = 6
+
+console.log("--------NEW Part 5-------")
+
+let num1 = 5
+let num2 = 3
+
+let sum = num1 + num2;
+console.log(sum)
+
+let difference = num1 - num2
+console.log(difference)
+
+let product = num1 * num2
+console.log(product)
+
+let divisions = num1 / num2
+console.log(divisions)
+
+let moduloOperator = num1 % num2
+console.log(moduloOperator)
+
+console.log("--------------------------")
+
+let num3 = 10
+let sumAll = num1 + num2 + num3
+console.log(sumAll)
+
+let subAndSubtract = num3 - (num1 + num2)
+console.log(subAndSubtract)
+
+let mulAndAdd = num3 + (num1 * num2)
+console.log(mulAndAdd)
+
+let DivideAndAdd = (num3 / num1) + num2
+console.log(DivideAndAdd)
+
+let avg = sumAll / 3;
+console.log(avg)
+
+let numOne = 8
+let numTwo = 6
+
+let NSum = numOne + numTwo
+console.log(NSum)
+
+let NDifference = numOne - numTwo
+console.log(NDifference)
+
+let Nproducts = numOne * numTwo
+console.log(Nproducts)
+
+let NDivision = numOne / numTwo
+console.log(NDivision)
+
+let NModulo = numOne % numTwo
+console.log(NModulo)
+
+console.log("--------------------------")
+
+let numThree = 16
+let NSumAll = numOne + numTwo + numThree
+console.log(NSumAll)
+
+let NSumAndSubtract = numThree - (numOne + numTwo)
+console.log(NSumAndSubtract)
+
+let NMulAndAdd = (numOne * numTwo) + numThree
+console.log(NMulAndAdd)
+
+let NDivideAndAdd = (numThree / numOne) + numTwo
+console.log(NDivideAndAdd)
+
+let NAvg = (numOne + numTwo + numThree) / 3
+console.log(NAvg)
+
+console.log("--------------------------")
+
+let numberOne = 10
+let numberTwo = 5
+let N1Sum = numberOne + numberTwo
+console.log(N1Sum)
+
+let N1Difference = numberOne - numberTwo 
+console.log(N1Difference)
+
+let N1Product = numberOne * numberTwo 
+console.log(N1Product)
+
+let N1Division = numberOne / numberTwo
+console.log(N1Division)
+
+let N1Modulo = numberOne % numberTwo
+console.log(N1Modulo)
+
+let numberThree = 25
+let N1SumAll = numberOne + numberTwo + numberThree
+console.log(N1SumAll)
+
+let N1AddAndSubtract = numberThree - (numberOne + numberTwo)
+console.log(N1AddAndSubtract)
+
+let N1MulAndAdd = (numberOne * numberTwo) + numberThree
+console.log(N1MulAndAdd)
+
+let N1DivideAndAdd = (numberThree / numberOne) + numberTwo
+console.log(N1DivideAndAdd)
+
+let N1Avg = (numberOne + numberTwo + numberThree) / 3
+console.log(N1Avg)
+
+console.log("--------Compareson Operators-------")
+
+let rohitAge = 22
+let rahuleAge = 23
+let isRohitOlder = rohitAge > rahuleAge
+console.log(isRohitOlder)
+
+let isRohitYounger = rohitAge < rahuleAge
+console.log(isRohitYounger)
+
+let romaAge = 22
+let isRohitEqualRoma = rohitAge === romaAge
+console.log(isRohitEqualRoma)
+
+let ninaAge = 23
+let isNinaOlderSameRahul = ninaAge >= rahuleAge
+console.log(isNinaOlderSameRahul)
+
+let isNinaOlderOrSameRohit = ninaAge >= rohitAge
+console.log(isNinaOlderOrSameRohit)
+
+let isRomaYoungerOrSameNina = romaAge <= ninaAge
+console.log(isRomaYoungerOrSameNina)
+
+console.log("--------------------------")
+
+let product1Price = 499
+let product2Price = 699
+
+let budget = 1199
+
+let totalPrice = product1Price + product2Price
+let areBothUnderBudget = budget > totalPrice
+console.log("Can I afford the two products?", areBothUnderBudget)
+
+console.log("--------------------------")
+
+let maths = 87
+let english = 33
+let science = 43
+
+let passingAverage = 60
+
+let average = (maths + english + science) / 3
+let isPass = average > passingAverage
+console.log("Did I pass?",isPass)
+
+console.log("--------------------------")
+
+let number = 50
+let isNumberDivisibleBy3 = number % 3 === 0
+
+let isNumberDivisibleBy7 = number % 7 === 0
+
+console.log("Divisible by 3",isNumberDivisibleBy3)
+console.log("Divisible by 7",isNumberDivisibleBy7)
+
+console.log("--------------------------")
+
+let akshayTime = 10.75
+let taviTime = 10.92
+let isAkshayFaster = akshayTime < taviTime
+console.log("Is Akshay faster than Ravi in the 100m sprint?",isAkshayFaster)
+
+let akshayTime400 = 46.21
+let raviTime400 = 47.05
+
+let isAkshayFaster400 = akshayTime400 < raviTime400
+console.log("Is Akshay faster than ravi in the 400m race?",isAkshayFaster400)
+
+let akshayDistance = 7.83
+let raviDistance = 7.68
+
+let isAkshayLongerJump = akshayDistance > raviDistance
+console.log("Did Akshay make a longer jump than Ravi?",isAkshayLongerJump)
+
+let akshayDistanceShotPut = 19.75
+let raviDistanceShotPut = 18.92
+
+let isAkshayLongerThrow = akshayDistanceShotPut > raviDistanceShotPut
+
+console.log("Did Akshay make a longer throw than Ravi?",isAkshayLongerThrow)
+
+let akshayHeight = 2.05
+let raviHeight = 1.98
+
+let isAkshayHigherJump = akshayHeight > raviHeight
+console.log("Did Akshay jump higher than Ravi",isAkshayHigherJump)
+
+console.log("--------------------------")
+
+let milkPrice = 199
+let breadPrice = 299
+
+let NBudget = 300
+
+let NtotalPrice = milkPrice + breadPrice
+let NareBothUnderBudget = NBudget > NtotalPrice
+console.log("Can I buy both Milk and Bread within my budget?",NareBothUnderBudget)
+
+
+let gangaCricketClubScore = 78
+let yamunaCricketClubScore = 63
+let narmadaCricketClubScore = 52
+let godavariCricketClubScore = 72
+
+let minimumAverage = 60
+let averageScore = (gangaCricketClubScore + yamunaCricketClubScore + narmadaCricketClubScore + godavariCricketClubScore) / 4
+let isSelected = minimumAverage < averageScore
+console.log("Am I selected fir the cricket camp?",isSelected)
+
+let numberOfStudent = 72
+let isDivisibleBy4 = numberOfStudent % 4 === 0
+let isDivisibleBy9 = numberOfStudent % 9 === 0
+console.log("Can the students be divided into groups of 4?",isDivisibleBy4)
+console.log("Can the students be divided into groups of 9?",isDivisibleBy9)
