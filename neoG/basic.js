a25794f65c0f38747825b38e8ae1d343d577d6e4
Shubham Vishwakarma <100425143+SHUBHAM77108@users.noreply.github.com>
2024-03-31 10:08:03 +0000

    commit

--- neoG/basic.js
+++ neoG/basic.js
@@ -307,6 +307,61 @@
 // let isAkshayHigherJump = akshayHeight > raviHeight
 // console.log("Did Akshay jump higher than Ravi",isAkshayHigherJump)
 
+// let rajeshRating = 8.2
+// let amitRating = 7.9
+
+// let isRajeshHigherRated = rajeshRating > amitRating
+// console.log("IS rajesh higher rated than amit?",isRajeshHigherRated)
+
+// let rajeshcollection = 250
+// let amitcollection = 350
+
+// let isRajeshHigherCollection = rajeshcollection > amitcollection
+// console.log("Does Rajesh have a higher superhit movie collection than amit?",isRajeshHigherCollection)
+
+// let rajeshIncome = 50
+// let amitIncome = 80
+
+// let isRajeshLessIncome = rajeshIncome < amitIncome
+// console.log("Does the rajesh have a lower yearly income than amit?",isRajeshLessIncome)
+
+// let rajeshAds = 10
+// let amitAds = 8
+
+// let isRajeshMoreAds= rajeshAds > amitAds
+// console.log("Has Rajesh done more ads than Amit?",isRajeshMoreAds)
+
+// let rajeshFans = 200000
+// let amitFans = 3500000
+
+// let isRajeshMoreFans = rajeshAds > amitFans
+// console.log("Does Rajesh have more fans the Amit?", isRajeshMoreFans)
+
+// console.log("--------------------------")
+
+// let tShirtPrice = 799
+// let jeansPrice = 599
+
+// let N1Budget = 1500
+// let NareBothUnderBudget = N1Budget > tShirtPrice + jeansPrice 
+
+// console.log("Can I buy both a T-shirt and Jeans within my budget?",NareBothUnderBudget)
+
+// let mathScore = 85
+// let englishScore = 92
+// let physicsScore = 78
+
+// let N1minimumAverage = 85
+// let N1averageScore = mathScore + englishScore + physicsScore / 3
+
+// let isQualified = N1averageScore >= N1minimumAverage
+// console.log("Am I qualified for the olympiad?", isQualified)
+
+// let productQuantity = 60
+// let isDivisibleBy5 = productQuantity % 5 === 0
+// let isDivisibleBy10 = productQuantity % 10 === 0
+// console.log("Can the product quantity be sold in batches of 5?",isDivisibleBy5)
+// console.log("Can the product quantity be sold in batches of 10?",isDivisibleBy10)
 // console.log("--------------------------")
 
 // let milkPrice = 199
@@ -335,95 +390,398 @@
 // console.log("Can the students be divided into groups of 4?",isDivisibleBy4)
 // console.log("Can the students be divided into groups of 9?",isDivisibleBy9)
 
-console.log("-------- Logical Operators-----------")
+// console.log("-------- Logical Operators-----------")
 
-let totalAverage = 87 
-let hasSportsEndorsement = true
+// let totalAverage = 87 
+// let hasSportsEndorsement = true
 
-let isEligibleForScholorship = totalAverage > 85 || hasSportsEndorsement
+// let isEligibleForScholorship = totalAverage > 85 || hasSportsEndorsement
 
-console.log("Is the students is eligible for scholarship", isEligibleForScholorship)
+// console.log("Is the students is eligible for scholarship", isEligibleForScholorship)
 
-let shoppingAmount = 1250
-let hasPrimeMembership = true
+// let shoppingAmount = 1250
+// let hasPrimeMembership = true
 
-let qualifiesForFreeShipping = shoppingAmount > 999 || hasPrimeMembership
-console.log("Does the user qualify for free shipping?",qualifiesForFreeShipping)
+// let qualifiesForFreeShipping = shoppingAmount > 999 || hasPrimeMembership
+// console.log("Does the user qualify for free shipping?",qualifiesForFreeShipping)
 
-let sbiBalance = 5000
-let hdfcBalance = 3000
-let sareeCost = 2500
-let kurtiCost = 1500
-let jewelryCost = 1000
+// let sbiBalance = 5000
+// let hdfcBalance = 3000
+// let sareeCost = 2500
+// let kurtiCost = 1500
+// let jewelryCost = 1000
 
-let totalCost = sareeCost + kurtiCost + jewelryCost;
-let isAffordable = sbiBalance >= totalCost || hdfcBalance >= totalCost
-console.log("Can you afford the purchase?", isAffordable)
+// let totalCost = sareeCost + kurtiCost + jewelryCost;
+// let isAffordable = sbiBalance >= totalCost || hdfcBalance >= totalCost
+// console.log("Can you afford the purchase?", isAffordable)
 
-console.log("--------------------")
+// console.log("--------------------")
 
-let shirtColor = "red"
-let shirtSize = "L"
-let shirtBrand = "Jockey"
+// let shirtColor = "red"
+// let shirtSize = "L"
+// let shirtBrand = "Jockey"
 
-let matchesFilters = shirtColor === "red" && shirtSize === "L" && shirtBrand === "Jockey" || "USPA"
-console.log("Does the shirt match the filters", matchesFilters)
+// let matchesFilters = shirtColor === "red" && shirtSize === "L" && shirtBrand === "Jockey" || "USPA"
+// console.log("Does the shirt match the filters", matchesFilters)
 
-let remainingItem = 4
-let itemSize = "M"
-let itemBrand = "Nike"
+// let remainingItem = 4
+// let itemSize = "M"
+// let itemBrand = "Nike"
 
-let isDiscountAppLicable = remainingItem < 5 && itemSize === "M" && (itemBrand === "Nike" || itemBrand === "Adidas")
+// let isDiscountAppLicable = remainingItem < 5 && itemSize === "M" && (itemBrand === "Nike" || itemBrand === "Adidas")
 
-console.log("Is the discount applicable:", isDiscountAppLicable)
+// console.log("Is the discount applicable:", isDiscountAppLicable)
+
+// console.log("--------------------------")
+
+// let subject1Marks = 85
+// let subject2Marks = 92
+// let subject3Marks = 78
+
+// let averageMarks = subject1Marks + subject2Marks + subject3Marks / 3
+// let hasDramaSchoolEndorsement = true
+
+// let studentsPerformance = averageMarks >= 80 || hasDramaSchoolEndorsement
+// console.log("Is the user selected?", studentsPerformance)
+
+// let foodBill = 1500
+// let hasZomatoGoldMembership = true
+
+// let qualifiesForDiscountedBill = foodBill < 2000 && hasZomatoGoldMembership
+// console.log("Does the user qualify to pay the food bill at discount price?", qualifiesForDiscountedBill)
+
+// let piggybank1Balance = 2000
+// let piggybank2Balance = 1500
+// let penCost = 50
+// let notebookCost = 100
+// let pencilCost = 20
+
+// let NtotalCost = penCost + notebookCost + pencilCost
+// let canWeAffordStationert = NtotalCost <= piggybank1Balance || NtotalCost <= piggybank2Balance
+// console.log("Can you Afford the purchase?",canWeAffordStationert)
+
+// console.log("--------------------------")
+
+// let personAge = 25
+// let isAgeInRange = personAge <= 60 && personAge >= 18
+
+// console.log("Is the person's age between 18 and 60?",isAgeInRange)
+
+// console.log("--------------------------")
+
+// let tvScreenSize = 55
+// let tvBrand = "Sony" 
+// let tvPrice = 50000
+
+// let meetsCriteria = tvScreenSize >= 50 && (tvBrand === "Sony" || tvBrand === "Samsung") && tvPrice <= 60000
+// console.log("Does the TV meet the criteria?", meetsCriteria)
+
+// let watchReleaseYear = 2021
+// let watchSize = "L"
+// let watchBrand = "Apple"
+
+// let NisDiscountApplicable = watchReleaseYear === 2020 && (watchSize === "M" || watchSize === "L") && (watchBrand === "Apple" || watchBrand === "Samsung")
+// console.log("Is the discount applicable?", NisDiscountApplicable)
+
+// console.log("--------------------------")
+
+// let runsMatch1 = 78
+// let runsMatch2 = 102
+// let runsMatch3 = 45
+
+// let totalRuns = runsMatch1 + runsMatch2 + runsMatch3
+// let battingAverage = totalRuns / 3
+
+// let excellentFielding = true
+// let cricketPerfomance = battingAverage > 60 && excellentFielding
+// console.log("Is the cricketer selected?", cricketPerfomance)
+
+// let gymMembership = true 
+// let purchaseAmount = 1500
+// let qualifiesForDiscountedPurchase = gymMembership && purchaseAmount > 1499
+// console.log("Does the user qualify to purchase the item at a discounted price?", qualifiesForDiscountedPurchase)
+
+// let iciciBalance = 8000
+// let axisBalance = 6000
+// let laptopCost = 35000
+// let phoneCost = 25000
+// let headphonesCost = 3000
+
+// let N1totalCost = laptopCost + phoneCost + headphonesCost
+
+// let NCanWeAffordElectronics = N1totalCost <= iciciBalance || N1totalCost <= axisBalance
+// console.log("Can you afford the purchase?", NCanWeAffordElectronics)
+
+// console.log("--------------------------")
+
+// let carSpeed = 65
+// let isSpeedInRange = carSpeed >= 40 && carSpeed <= 80
+// console.log("IS the car's speed between 40 and 80?",isSpeedInRange)
+
+// console.log("--------------------------")
+
+// let phoneMemory = 64
+// let phonePrice = 25000
+// let phoneBrand = "OnePlus"
+
+// let NmeetsCriteria = phoneMemory < 128 && phoneCost >= 35000 && (phoneBrand === "OnePlus" || phoneBrand === "Samsung")
+// console.log("Does the phone meet the criteria?",NmeetsCriteria)
+
+// let NremainingItem = 4
+// let shoesSize = "M"
+// let shoeBrand = "Nike"
+
+// let NIsDiscountApplicable = NremainingItem < 5 && shoesSize === "M" && (shoeBrand === "Nike" || shoeBrand === "Adidas")
+// console.log("Is the discount applicable?", NIsDiscountApplicable)
 
 console.log("--------------------------")
 
-let subject1Marks = 85
-let subject2Marks = 92
-let subject3Marks = 78
+let originalPrice = 1000
 
-let averageMarks = subject1Marks + subject2Marks + subject3Marks / 3
-let hasDramaSchoolEndorsement = true
+let discountPercentage = 25
 
-let studentsPerformance = averageMarks >= 80 || hasDramaSchoolEndorsement
-console.log("Is the user selected?", studentsPerformance)
+let discountAmount = (originalPrice * discountPercentage) / 100
 
-let foodBill = 1500
-let hasZomatoGoldMembership = true
+let finalPrice = discountAmount - originalPrice
+console.log("Original Price:",originalPrice)
+console.log("Discount Percentage:",discountPercentage)
+console.log("Discount Amount:",discountAmount)
+console.log("Final Price:",finalPrice)
+console.log("--------------------------")
 
-let qualifiesForDiscountedBill = foodBill < 2000 && hasZomatoGoldMembership
-console.log("Does the user qualify to pay the food bill at discount price?", qualifiesForDiscountedBill)
+let itemOne = "T-shirt"
+let itemTwo = "Jeans"
+let itemThree = "Shoes"
 
-let piggybank1Balance = 2000
-let piggybank2Balance = 1500
-let penCost = 50
-let notebookCost = 100
-let pencilCost = 20
+let itemOnePrice = 500
+let itemTwoPrice = 1500
+let itemThreePrice = 2000
 
-let NtotalCost = penCost + notebookCost + pencilCost
-let canWeAffordStationert = NtotalCost <= piggybank1Balance || NtotalCost <= piggybank2Balance
-console.log("Can you Afford the purchase?",canWeAffordStationert)
+let itemOneQuantity = 2
+let itemTwoQuantity = 1
+let itemThreeQuantity = 1
+
+let totalItemOnePrice = itemOnePrice * itemOneQuantity
+let totalItemTwoPrice = itemTwoPrice * itemTwoQuantity
+let totalItemThreePrice = itemThreePrice * itemThreeQuantity
+
+let totalcartPrice = totalItemOnePrice + totalItemTwoPrice + totalItemThreePrice
+
+console.log("item One:",itemOne)
+console.log("item Two:",itemTwo)
+console.log("item Three:",itemThree)
+console.log("--------------------------")
+console.log("Price of",itemOne,"x",itemOneQuantity,":",totalItemOnePrice)
+console.log("Price of",itemTwo,"x",itemTwoQuantity,":",totalItemTwoPrice)
+console.log("Price of",itemThree,"x",itemThreeQuantity,":",totalItemThreePrice)
+console.log("--------------------------")
+console.log("Total Cart Price:",totalcartPrice)
+
+let income1 = 50000
+let income2 = 30000
+
+let totalIncome = income1 + income2
+
+let expense1 = 15000
+let expense2 = 10000
+let expense3 = 8000
+let expense4 = 12000
+
+let totalExpemses = expense1 + expense2 + expense3 + expense4
+
+let balance = totalIncome - totalExpemses
+
+console.log("Monthly Income 1 ₹",income1)
+console.log("Monthly Income 2 ₹",income2)
+console.log("--------------------------")
+console.log("Total Income:",totalIncome)
+console.log("--------------------------")
+console.log("Monthly Expense 1: ₹",expense1)
+console.log("Monthly Expense 2: ₹",expense2)
+console.log("Monthly Expense 3: ₹",expense3)
+console.log("Monthly Expense 4: ₹",expense4)
+console.log("--------------------------")
+console.log("Total Expenses: ₹",totalExpemses)
+console.log("--------------------------")
+console.log("Balance: ₹",balance)
 
 console.log("--------------------------")
 
-let personAge = 25
-let isAgeInRange = personAge <= 60 && personAge >= 18
+let sales1 = 50000
+let sales2 = 60000
+let sales3 = 45000
+let sales4 = 55000
 
-console.log("Is the person's age between 18 and 60?",isAgeInRange)
+let totalSales = sales1 + sales2 + sales3 + sales4
+
+let rentExpense = 25000
+let salaryExpense = 18000
+let inventoryExpense = 12000
+let utilityExpense = 15000
+
+let NtotalExpenses = rentExpense + salaryExpense + inventoryExpense + utilityExpense
+
+let netIncome =totalSales - NtotalExpenses 
+
+console.log("Kirana Store Income Statement")
+console.log("--------------------------")
+console.log("Total Sales:",totalSales)
+console.log("--------------------------")
+console.log("Rent Expense:",rentExpense)
+console.log("Salary Expense:",salaryExpense)
+console.log("Inventory Expense:",inventoryExpense)
+console.log("Utility Expense:",15000)
+console.log("--------------------------")
+console.log("Total Expense:",NtotalExpenses)
+console.log("--------------------------")
+console.log("Net Income:",netIncome)
+console.log("--------------------------")
+
+let book1Quantity = 25
+let book2Quantity = 15
+let book3Quantity = 20
+
+let book1Price = 350
+let book2Price = 250
+let book3Price = 200
+
+let book1Value = book1Quantity * book1Price
+let book2Value = book2Quantity * book2Price
+let book3Value = book3Quantity * book3Price
+
+let totalInventoryValue = book1Value + book2Value + book3Value
+
+console.log("Indian Bookstore Inventory Report")
+console.log("--------------------------")
+console.log("Book 1: Mahabharata")
+console.log("Quantity:",book1Quantity)
+console.log("Price:",book1Price)
+console.log("value:",book1Value)
+
+console.log("Book 2: Ramayana")
+console.log("Quantity:",book2Quantity)
+console.log("Price:",book2Price)
+console.log("value:",book2Value)
+
+console.log("Book 3: Gitanjali")
+console.log("Quantity:",book3Quantity)
+console.log("Price:",book3Price)
+console.log("value:",book3Value)
+
+console.log("Total Inventory Value:",totalInventoryValue)
 
 console.log("--------------------------")
 
-let tvScreenSize = 55
-let tvBrand = "Sony" 
-let tvPrice = 50000
+let contractor1 = "Rajesh"
+let contractor2 = "Priya"
+let contractor3 = "Amit"
+let contractor4 = "Ananya"
 
-let meetsCriteria = tvScreenSize >= 50 && (tvBrand === "Sony" || tvBrand === "Samsung") && tvPrice <= 60000
-console.log("Does the TV meet the criteria?", meetsCriteria)
+let jobDescription1 = "Web Developer"
+let jobDescription2 = "Content Writer"
+let jobDescription3 = "Graphic Designer"
+let jobDescription4 = "Digital Marketer"
 
-let watchReleaseYear = 2021
-let watchSize = "L"
-let watchBrand = "Apple"
+let hourlyRate1 = 300
+let hourlyRate2 = 250
+let hourlyRate3 = 350
+let hourlyRate4 = 200
 
-let NisDiscountApplicable = watchReleaseYear === 2020 && (watchSize === "M" || watchSize === "L") && (watchBrand === "Apple" || watchBrand === "Samsung")
-console.log("Is the discount applicable?", NisDiscountApplicable)
+let hoursWorked1 = 80
+let hoursWorked2 = 60
+let hoursWorked3 = 40
+let hoursWorked4 = 20
+
+let ratings1 = 90
+let ratings2 = 80
+let ratings3 = 92
+let ratings4 = 88
+
+let isEligibleForGoodieBag1 = hoursWorked1 > 15 || ratings1 > 85
+let isEligibleForGoodieBag2 = hoursWorked1 > 15 || ratings1 > 85
+let isEligibleForGoodieBag3 = hoursWorked1 > 15 || ratings1 > 85
+let isEligibleForGoodieBag4 = hoursWorked1 > 15 || ratings1 > 85
+
+let totalPaymentContractor1 = hourlyRate1 * hoursWorked1
+let totalPaymentContractor2 = hourlyRate2 * hoursWorked2
+let totalPaymentContractor3 = hourlyRate3 * hoursWorked3
+let totalPaymentContractor4 = hourlyRate4 * hoursWorked4
+
+console.log("Payment Chart:")
+console.log("--------------------------")
+console.log("Contractor:",contractor1)
+console.log("Job Description:",jobDescription1)
+console.log("Hourly Rate:",hourlyRate1)
+console.log("Hours Worked:",hoursWorked1)
+console.log("Total Payment:",totalPaymentContractor1)
+console.log("Ratings:",ratings1)
+console.log("Is Eligible for Goodie Bag:",isEligibleForGoodieBag1)
+
+console.log("--------------------------")
+console.log("Contractor:",contractor2)
+console.log("Job Description:",jobDescription2)
+console.log("Hourly Rate:",hourlyRate2)
+console.log("Hours Worked:",hoursWorked2)
+console.log("Total Payment:",totalPaymentContractor2)
+console.log("Ratings:",ratings2)
+console.log("Is Eligible for Goodie Bag:",isEligibleForGoodieBag2)
+
+console.log("--------------------------")
+console.log("Contractor:",contractor3)
+console.log("Job Description:",jobDescription3)
+console.log("Hourly Rate:",hourlyRate3)
+console.log("Hours Worked:",hoursWorked3)
+console.log("Total Payment:",totalPaymentContractor3)
+console.log("Ratings:",ratings3)
+console.log("Is Eligible for Goodie Bag:",isEligibleForGoodieBag3)
+
+console.log("--------------------------")
+console.log("Contractor:",contractor4)
+console.log("Job Description:",jobDescription4)
+console.log("Hourly Rate:",hourlyRate4)
+console.log("Hours Worked:",hoursWorked4)
+console.log("Total Payment:",totalPaymentContractor4)
+console.log("Ratings:",ratings4)
+console.log("Is Eligible for Goodie Bag:",isEligibleForGoodieBag4)
+
+console.log("--------------------------")
+
+let movie1TicketsSold = 150
+let movie2TicketsSold = 120
+let movie3TicketsSold = 80
+
+let movie1TicketPrice = 250
+let movie2TicketPrice = 180
+let movie3TicketPrice = 150
+
+let movie1Revenue = movie1TicketsSold * movie1TicketPrice
+let movie2Revenue = movie2TicketsSold * movie2TicketPrice
+let movie3Revenue = movie3TicketsSold * movie3TicketPrice
+
+let isMovie1Superhit = movie1Revenue > 5000
+let isMovie2Superhit = movie2Revenue > 5000
+let isMovie3Superhit = movie3Revenue > 5000
+
+let totalRevenue = movie1Revenue + movie2Revenue + movie3Revenue
+
+console.log("Indian Movie Ticket Sales Report")
+console.log("--------------------------")
+console.log("Movie 1: Dilwale Dulhania Le Jayenge")
+console.log("Tickets Sold:",movie1TicketsSold)
+console.log("Ticket Price:",movie1TicketPrice)
+console.log("Revenue:",movie1Revenue)
+console.log("Superhit:",isMovie1Superhit)
+
+console.log("Movie 2: Kabhi Khushi Kabhie Gham")
+console.log("Tickets Sold:",movie2TicketsSold)
+console.log("Ticket Price:",movie2TicketPrice)
+console.log("Revenue:",movie2Revenue)
+console.log("Superhit:",isMovie2Superhit)
+
+console.log("Movie 3: 3 Idiots")
+console.log("Tickets Sold:",movie3TicketsSold)
+console.log("Ticket Price:",movie3TicketPrice)
+console.log("Revenue:",movie3Revenue)
+console.log("Superhit:",isMovie3Superhit)
+
+console.log("Total Revenue:",totalRevenue)
