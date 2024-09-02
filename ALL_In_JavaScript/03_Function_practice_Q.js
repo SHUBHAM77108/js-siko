610db6c04cfb52b305530b8ee209b1f554e5a5b1
Shubham Vishwakarma <100425143+SHUBHAM77108@users.noreply.github.com>
2024-09-02 12:45:10 +0000

    1 commit on 02 Sep 24

--- ALL_In_JavaScript/03_Function_practice_Q.js
+++ ALL_In_JavaScript/03_Function_practice_Q.js
@@ -1,208 +1,272 @@
 // JavaScript Function Practice Questions 
 
 
-console.log('1 >-------------------');
+// console.log('1 >-------------------');
 
 // 1 >---Question:
 // Write a function named isEven that takes a number as an argument and returns true if the number is even, and false if it is odd. 
 
 
-function isEven (number) {
-    if (number % 2 !== 0) {
-        return `${number} is an even number.`;
-    } else {
-        return `is an odd ${number}`;
-    }
-}
+// function isEven (number) {
+//     if (number % 2 !== 0) {
+//         return `${number} is an even number.`;
+//     } else {
+//         return `is an odd ${number}`;
+//     }
+// }
 
-console.log(isEven(5));
+// console.log(isEven(5));
 
-console.log('2 >-------------------');
+// console.log('2 >-------------------');
 
 // 2 >---Question:
 // Write a function named getGreeting that takes a string (name) as an argument and returns a greeting message like "Hello, [name]!".
 
-function getGreeting (greeting){
-    console.log(`Hello, ${greeting}`);
-}
+// function getGreeting (greeting){
+//     console.log(`Hello, ${greeting}`);
+// }
 
-getGreeting('Shubham');
+// getGreeting('Shubham');
 
-console.log('3 >-------------------');
+// console.log('3 >-------------------');
 
 // 3 >---Question:
 // Write a function named isAdult that takes an age as an argument and returns true if the age is 18 or above, and false if it is less than 18.
 
-function isAdult (name,adult) {
-    // if (adult >= 16) {
-    //     return `${name}, is an a adult person.`
-    // } else {
-    //     return `${name}, is not an a adult person.`
-    // }
-console.log(adult >= 16 ? `${name}, is an a adult person.` : `${name}, is not an a adult person.`);
+// function isAdult (name,adult) {
+//     // if (adult >= 16) {
+//     //     return `${name}, is an a adult person.`
+//     // } else {
+//     //     return `${name}, is not an a adult person.`
+//     // }
+// console.log(adult >= 16 ? `${name}, is an a adult person.` : `${name}, is not an a adult person.`);
 
-}
+// }
 
-isAdult('shubham', 18);
-isAdult('abhi', 15);
+// isAdult('shubham', 18);
+// isAdult('abhi', 15);
 
-console.log('4 >-------------------');
+// console.log('4 >-------------------');
 
 // 4 >---Question:
 // Write a function named add that takes two numbers as arguments and returns their sum. Then, write another function named calculate that calls the add function with two numbers and returns the result.
 
-function AddTakes (num1,num2){
-    let sum = num1 + num2;
-    return sum;
-}
+// function AddTakes (num1,num2){
+//     return  num1 + num2;
+// }
 
-function calculateSumofnumber (anothernum1, anothernum2) {
-    let AddTakes = calculateSumofnumber;
-    return AddTakes;
-}
+// function calculateSumofnumber () {
+//     return AddTakes(5,10); // Example of calling AddTakes function
+// }
 
-console.log(calculateSumofnumber(5, 6));
+// console.log(calculateSumofnumber());
 
-console.log('5 >-------------------');
+// console.log('5 >-------------------');
 
 // 5 >---Question:
 // Write a function named isPositive that takes a number as an argument and returns true if the number is positive, false if it is negative, and null if it is zero.
 
 
 // i use a truthy and false value 
-function isPositiveNumber (num) {
-    // if (num >= 0) {
-    //     console.log(`${num} is a true`);
-    // } else {
-    //     console.log(`${num} is a false value`);
-    // }
+// function isPositiveNumber (num) {
+//     // if (num > 0) {
+//     //     console.log(`${num} is a true`);
+//     // } else if (num < 0) {
+//     //     console.log(`${num} is a false value`);
+//     // } else {
+//     //     return null;
+//     // }
 
-    // same output using trenary operator
-    num >= 0 ? console.log(`${num} is a true`) : console.log(`${num} is a false`)
-}
-isPositiveNumber(1);
+//     // same output using trenary operator
+//     num > 0 ? console.log(`${num} is a true`) : console.log(`${num} is a false`)
+// }
+// isPositiveNumber(1);
 
 
-console.log('6 >-------------------');
+// console.log('6 >-------------------');
 
 // 6 >---Question:
 // Write a function named getLength that takes a string as an argument and returns the length of the string. Then, write another function named compareLengths that takes two strings, calls getLength for both, and returns the longer string.
 
-// function getLength () {
-
+// function getLength (str) {
+//     return str.length;
 // }
 
-console.log('7 >-------------------');
+// function compareLengths (str1, str2) {
+//     if (getLength(str1) > getLength(str2)) {
+//         return str1;
+//     } else {
+//         return str2;
+//     }
+// }
+// console.log(compareLengths('shubham','abhishekh'));
+
+// console.log('7 >-------------------');
 
 // 7 >---Question
 // Write a function named isEqual that takes two arguments and returns true if they are equal and false if they are not.
 
-function isEqual (value1, value2) {
-    return value1 === value2 ? `Both ${value1}, ${value2} are equal: true` : `Both ${value1}, ${value2} are not equal value: false`;
-}
+// function isEqual (value1, value2) {
+//     return value1 === value2 ? `Both ${value1}, ${value2} are equal: true` : `Both ${value1}, ${value2} are not equal value: false`;
+// }
 
-console.log(isEqual(5,'5'));
-console.log(isEqual(5,"'shubham'"));
+// console.log(isEqual(5,'5'));
+// console.log(isEqual(5,"'shubham'"));
 
 
-console.log('8 >-------------------');
+// console.log('8 >-------------------');
 
 // 8 >---Questions
 // Write a function named getFirstCharacter that takes a string as an argument and returns the first character of the string. Then, write another function named isFirstCharacterA that uses getFirstCharacter and checks if the first character is 'A'.
 
-// function getFirstCharacter () {
-
+// function getFirstCharacter (string) {
+//     return string.charAt(0);
 // }
 
-console.log('9 >-------------------');
+// function isFirstCharacterA (string2) {
+//     return getFirstCharacter(string2) === 'A'
+// }
+// console.log(isFirstCharacterA('Abhi'))
+
+// console.log('9 >-------------------');
 
 // 9 >---Questions
 // Write a function named multiply that takes two numbers as arguments and returns their product. Then, write another function named square that uses multiply to return the square of a number.
 
-console.log('10 >-------------------');
+// function getMultiply (num1, num2) {
+//     return num1 * num2;
+// }
+
+// function getsquare (num) {
+//     return getMultiply (num, num);
+// }
+// console.log(getsquare(6,4));
+
+// console.log('10 >-------------------');
 
 // 10 >---Questions
 // Write a function named isLongerThan that takes two strings as arguments and returns true if the first string is longer than the second, and false otherwise.
 
-function isLongerThanTwo (string1, string2) {
-    let stringcount1 = 0;
-    for (let i = 0; i < string1.length; i++) {
-        stringcount1 += 1;
-    }
+// function isLongerThanTwo (string1, string2) {
+//     let stringcount1 = 0;
+//     for (let i = 0; i < string1.length; i++) {
+//         stringcount1 += 1;
+//     }
 
-    let stringcount2 = 0;
-    for (let i = 0; i < string2.length; i++) {
-        stringcount2 += 1;
-    }
-    return (stringcount2);
-}
-console.log(isLongerThanTwo("shubham","abhishekh"));
+//     let stringcount2 = 0;
+//     for (let i = 0; i < string2.length; i++) {
+//         stringcount2 += 1;
+//     }
+//     return stringcount1, stringcount2;
+// }
 
-console.log('11 >-------------------');
+
+// shortcut way 
+
+// function isLongerThanTwo (str1, str2) {
+//     return str1.length > str2.length;
+// }
+// console.log(isLongerThanTwo("shubham","abhishekh"));
+
+// console.log('11 >-------------------');
 
 // 11 >---Question
 // Write a function named isUpperCase that takes a string as an argument and returns true if the string is in uppercase, and false otherwise.
 
-function isUpperCase (isUpperCaseletter) {
-   return isUpperCaseletter === isUpperCaseletter.toUpperCase() ? "The String is a UpperCase letter :True" : "The String is not a UpperCase letter :False";
-}
+// function isUpperCase (isUpperCaseletter) {
+//    return isUpperCaseletter === isUpperCaseletter.toUpperCase() ? "The String is a UpperCase letter :True" : "The String is not a UpperCase letter :False";
+// }
 
-console.log(isUpperCase('shubham')); //False
-console.log(isUpperCase('SHUBHAM')); //True
+// console.log(isUpperCase('shubham')); //False
+// console.log(isUpperCase('SHUBHAM')); //True
 
-console.log('12 >-------------------');
+// console.log('12 >-------------------');
 
 // 12 >---Question:
 // Write a function named containsSubstring that takes two strings as arguments and returns true if the second string is a substring of the first, and false otherwise.
 
-function containsSubstring (string1, string2){
-    return string1.charAt(0) === string2.charAt(0) ? "The First charater of string one and two both are same: True" : "The First charater of string one and two both are not same: False";
-}
+// function containsSubstring (string1, string2){
+//     return string1.charAt(0) === string2.charAt(0) ? "The First charater of string one and two both are same: True" : "The First charater of string one and two both are not same: False";
 
-console.log(containsSubstring('Summt', 'Summan'));
-console.log(containsSubstring('Summt', 'dumman'));
+//     // return string1.indexOf(string2) !== -1; // short way
+// }
 
-console.log('13 >-------------------');
+// console.log(containsSubstring('Summt', 'Summan'));
+// console.log(containsSubstring('Summt', 'dumman'));
+
+// console.log('13 >-------------------');
 
 // 13 >---Question:
 // Write a function named toUpperCaseString that takes a string as an argument and returns the string in uppercase.
 
-function toUpperCaseString2 (uppercase) {
-    return uppercase.toUpperCase();
-}
-console.log(toUpperCaseString2("amit")); // AMIT
+// function toUpperCaseString2 (uppercase) {
+//     return uppercase.toUpperCase();
+// }
+// console.log(toUpperCaseString2("amit")); // AMIT
 
-console.log('14 >-------------------');
+// console.log('14 >-------------------');
 
 // 14 >---Question
 // Write a function named isDivisibleBy that takes two numbers as arguments and returns true if the first number is divisible by the second, and false otherwise.
 
-function isDivisibleBy (num1, num2) {
- return num1 % num2 === 0 ? `${num1} is divisible by ${num2}` : `${num1} is not divisible by ${num2}`
-}
+// function isDivisibleBy (num1, num2) {
+//  return num1 % num2 === 0 ? `${num1} is divisible by ${num2}` : `${num1} is not divisible by ${num2}`
+// }
 
-console.log(isDivisibleBy(10, 2));
-console.log(isDivisibleBy(10, 3));
+// console.log(isDivisibleBy(10, 2));
+// console.log(isDivisibleBy(10, 3));
 
 
-console.log('15 >-------------------');
+// console.log('15 >-------------------');
 
 // 15 >---Question
 // Write a function named getLastCharacter that takes a string as an argument and returns the last character of the string. Then, write another function named isLastCharacterB that uses getLastCharacter and checks if the last character is 'B'.
 
-function getLastCharacter (stringValue) {
-    let result = stringValue.charAt(stringValue.length -1);
-    return result;
-}
+// function getLastCharacter (stringValue) {
+//     return stringValue.charAt(stringValue.length -1);
+// }
 
-function isLastCharacterB () {
-    if (getLastCharacter === 'B') {
-        return `Last Character is B`;
+// function isLastCharacterB (stringValue) {
+//     return getLastCharacter(stringValue) === 'B';
+// }
+
+// console.log(isLastCharacterB("SubhamB"));
+// console.log('16 >-------------------');
+
+
+// Here are more similar questions focusing on creating one function and using it in another:
+
+// 16 >---Questions
+// Write a function named getMiddleCharacter that takes a string as an argument and returns the middle character of the string. If the string has an even number of characters, return the two middle characters. Then, write another function named isMiddleCharacterVowel that uses getMiddleCharacter to check if the middle character(s) is a vowel (a, e, i, o, u).
+
+function getMiddleCharacter (str) {
+    let strcounter = 0;
+    for (let i = 0; i < str.length; i++){
+        strcounter += 1;
+    }
+    if (strcounter % 2 === 0) {
+        strcounter = strcounter / 2;
+        let firstChar = strcounter -1;
+        let secondChar = strcounter;
+        let result = str.charAt(firstChar) + str.charAt(secondChar);
+        return result;
     } else {
-        return `Last Character is not a B`
+        strcounter = (strcounter / 2) - 0.5;
+        return str.charAt(strcounter);
     }
 }
 
-getLastCharacter('shubham');
-console.log(isLastCharacterB());
-console.log('16 >-------------------');
+function isMiddleCharacterVowel (str) {
+    let strvowelAurNot = '';
+    for (let i = 0; i < getMiddleCharacter(str).length; i++) {
+
+        if (getMiddleCharacter(str).charAt(i) === "a" || getMiddleCharacter(str).charAt(i) === "e" || getMiddleCharacter(str).charAt(i) === "i" || getMiddleCharacter(str).charAt(i) === "o" || getMiddleCharacter(str).charAt(i) === "u") {
+            strvowelAurNot += `${getMiddleCharacter(str).charAt(i)} The middle character is a vowel\n`
+        } else {
+            strvowelAurNot += `${getMiddleCharacter(str).charAt(i)} The middle character is not a vowel\n`
+        }
+    }
+    return strvowelAurNot;
+} 
+
+console.log(isMiddleCharacterVowel('hamivoishu'));
