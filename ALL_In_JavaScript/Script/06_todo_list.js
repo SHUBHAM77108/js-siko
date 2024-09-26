14a0f9d2b02fa129c91a1a7ad95a995efe717ed7
Shubham Vishwakarma <subhamvishwakarma32@gmail.com>
2024-09-26 18:58:59 +0530

    02 commit on 26 SEP 24

--- ALL_In_JavaScript/Script/06_todo_list.js
+++ ALL_In_JavaScript/Script/06_todo_list.js
@@ -1,26 +1,22 @@
-const todoList = [{
-    name: 'wash Dishes',
-    duedate: '2022-12-22'
-}];
+// const todoList = [];
 
 renderTodoList();
 
 function renderTodoList() {
     let todoListHtml = '';
-    for(let i = 0; i < todoList.length; i++) {
-        const todoObject = todoList[i];
+    let retrivetodolist = JSON.parse(localStorage.getItem('todoList')) || [];
+    for(let i = 0; i < retrivetodolist.length; i++) {
+        const todoObject = retrivetodolist[i];
         const name = todoObject.name;
         const duedate = todoObject.duedate;
         const html = `<div>${name}</div> 
-        <div>${todoObject.duedate}</div>
+        <div>${duedate}</div>
         <button onclick="
-            todoList.splice(${i}, 1);
+            removeTodoListOnLocal(${i});
             renderTodoList();
             " class="delete-todo-button">Delete</button>`;
         todoListHtml += html;
     };
-
-    console.log(todoListHtml);
 
     document.querySelector('.js-todo-list').innerHTML = todoListHtml;
 }
@@ -30,9 +26,13 @@ function addTodo() {
     const getTodoName = document.querySelector('.js-name-input');
     const getDuedate = document.querySelector('.js-due-date-input');
 
+    // let retrivetodolist = JSON.parse(localStorage.getItem('todoList'));
+
     const duedate = getDuedate.value;
 
     const name = getTodoName.value;
+
+    let todoList = JSON.parse(localStorage.getItem('todoList')) || [];
 
     todoList.push({
         //name: name, //if the property and variable name are same we use one shortcut, only type property
@@ -41,6 +41,24 @@ function addTodo() {
         duedate
     });
 
+    localStorage.setItem('todoList',JSON.stringify(todoList));
+
     getTodoName.value = '';
+    getDuedate.value = '';
+
     renderTodoList()
+}
+
+// function updateTodoListOnLocal() {
+//     localStorage.setItem('todoList',JSON.stringify(todoList));
+// }
+
+function removeTodoListOnLocal(indexValue) {
+    let retrivetodoListToLocal = JSON.parse(localStorage.getItem('todoList'));
+
+    if (retrivetodoListToLocal) {
+        retrivetodoListToLocal.splice(indexValue, 1);
+
+        localStorage.setItem('todoList', JSON.stringify(retrivetodoListToLocal));
+    }
 }
