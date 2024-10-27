9eb173219a88cd533475796cf30df8d2a3c77129
Shubham Vishwakarma <subhamvishwakarma32@gmail.com>
2024-10-27 21:02:40 +0530

    commit on 27 OCT 24

--- ALL_In_JavaScript/08_Amazon-Project/javascript-amazon-project-main/javascript-amazon-project-main/scripts/checkout.js
+++ ALL_In_JavaScript/08_Amazon-Project/javascript-amazon-project-main/javascript-amazon-project-main/scripts/checkout.js
@@ -1,27 +1,26 @@
-import {cart, removeFromCart} from '../data/cart.js';
-import { products } from '../data/products.js';
-import { formatCurrency } from './utils/money.js';
+import { cart, removeFromCart } from "../data/cart.js";
+import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
-console.log(cart.quantity)
+// console.log(cart);
 
+let cartSummaryHTML = "";
 
-let cartSummaryHTML = '';
+cart.forEach((cartItem) => {
+  const productId = cartItem.productId;
 
-cart.forEach((cartItem)=> {
-    const productId = cartItem.productId;
+  let matchingProduct;
 
-    let matchingProduct;
+  products.forEach((product) => {
+    if (product.id === productId) {
+      matchingProduct = product;
+    }
+  });
 
-    products.forEach((product)=> {
-        if (product.id === productId) {
-            matchingProduct = product;
-        }
-    });
-
-
-
-    cartSummaryHTML += `          
-    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+  cartSummaryHTML += `          
+    <div class="cart-item-container js-cart-item-container-${
+      matchingProduct.id
+    }">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -39,12 +38,16 @@ cart.forEach((cartItem)=> {
             </div>
             <div class="product-quantity">
                 <span>
-                Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+                Quantity: <span class="quantity-label">${
+                  cartItem.quantity
+                }</span>
                 </span>
                 <span class="update-quantity-link link-primary">
                 Update
                 </span>
-                <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+                <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${
+                  matchingProduct.id
+                }">
                 Delete
                 </span>
             </div>
@@ -98,15 +101,40 @@ cart.forEach((cartItem)=> {
     </div>`;
 });
 
-document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+let cartQuantity = 0;
 
+function removeFromCartQuantity() {
+  cart.map((cartItem) => {
+    const productId = cartItem.dataset.productId;
+    if (cartItem.productId === productId) {
+      cartQuantity -= cartItem.quantity;
+    }
+  });
+}
 
-document.querySelectorAll('.js-delete-link').forEach((link) => {
-    link.addEventListener('click', ()=> {
-        const productId = link.dataset.productId;
-        removeFromCart(productId);
+document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
 
-        const container = document.querySelector(`.js-cart-item-container-${productId}`);
-        container.remove();
-    });
+document.querySelectorAll(".js-delete-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;
+    removeFromCart(productId);
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
+    UpdateCartQuantityIncheckoutPage();
+    console.log(removeFromCartQuantity());
+  });
 });
+
+UpdateCartQuantityIncheckoutPage();
+function UpdateCartQuantityIncheckoutPage() {
+  cart.forEach((cartItem) => {
+    cartQuantity = Number(cartQuantity + cartItem.quantity);
+    document.querySelector(
+      ".js-checkoutCartItam"
+    ).innerHTML = `Checkout (<a class="return-to-home-link" href="amazon.html">${cartQuantity} items</a>)`;
+    console.log(cartItem);
+  });
+}
