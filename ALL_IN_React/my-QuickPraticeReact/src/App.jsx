621578f7876f32ad21886060cb3b55830448d601
Shubham Vishwakarma <subhamvishwakarma32@gmail.com>
2025-12-30 20:54:23 +0530

    learn hook in react JS

--- ALL_IN_React/my-QuickPraticeReact/src/App.jsx
+++ ALL_IN_React/my-QuickPraticeReact/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function counter() {}
 
+const App = () => {
+  const [counter, setcounter] = useState(0);
+
+  function increaseCount() {
+    setcounter(counter + 1);
+  }
+  function decreaseCount() {
+    setcounter(counter - 1);
+  }
+  function jumpBy5() {
+    setcounter(counter + 5);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <div>
+      <div>{counter}</div>
+      <button onClick={increaseCount}>increase</button>
+      <button onClick={decreaseCount}>decrease</button>
+      <button onClick={jumpBy5}>jump by 5</button>
+    </div>
+  );
+};
+export default App;
