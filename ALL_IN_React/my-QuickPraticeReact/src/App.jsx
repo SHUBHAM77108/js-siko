789904b4fa3357df0a7ce266d874414dc9d8f774
Shubham Vishwakarma <subhamvishwakarma32@gmail.com>
2025-12-31 16:01:37 +0530

    start

--- ALL_IN_React/my-QuickPraticeReact/src/App.jsx
+++ ALL_IN_React/my-QuickPraticeReact/src/App.jsx
@@ -3,10 +3,12 @@ import { useState } from "react";
 function counter() {}
 
 const App = () => {
-  const [counter, setcounter] = useState(0);
+  const [counter, setcounter] = useState(10);
 
   function increaseCount() {
-    setcounter(counter + 1);
+    setcounter((prave) => prave + 1);
+    setcounter((prave) => prave + 1);
+    setcounter((prave) => prave + 1);
   }
   function decreaseCount() {
     setcounter(counter - 1);
@@ -15,11 +17,25 @@ const App = () => {
     setcounter(counter + 5);
   }
   return (
-    <div>
-      <div>{counter}</div>
-      <button onClick={increaseCount}>increase</button>
-      <button onClick={decreaseCount}>decrease</button>
-      <button onClick={jumpBy5}>jump by 5</button>
+    <div className="text-white p-5">
+      <div className="w-3xs h-28 text-8xl text-center p-5 bg-amber-800 font-bold">
+        {counter}
+      </div>
+      <button
+        className="w-30 h-20 pr-2 bg-amber-500 font-bold"
+        onClick={increaseCount}
+      >
+        increase
+      </button>
+      <button
+        className="w-30 h-20 pr-2 bg-amber-500 font-bold"
+        onClick={decreaseCount}
+      >
+        decrease
+      </button>
+      <button className="w-30 h-20 bg-amber-500 font-bold" onClick={jumpBy5}>
+        jump by 5
+      </button>
     </div>
   );
 };
